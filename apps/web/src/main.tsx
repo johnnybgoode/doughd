3ad436d6ad3a250258f6f723dd9aa2b9886388ef
John Entwistle <j.entwistle76@gmail.com>
2026-02-10 22:54:44 -0500

    feat: add recipe fixture factories and handlers, set up browser mocking

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,31 @@
 import './global.css';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router';
 import { App } from './components/App';
+import queryClient from './lib/queryClient';
 
-createRoot(document.getElementById('app')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+const enableMocking = async () => {
+  if (!(import.meta.env.DEV && import.meta.env.VITE_MOCK_SERVER === 'true')) {
+    return;
+  }
+  const { worker } = await import('./../__tests__/utils/setupBrowserWorker');
+  worker.start({
+    onUnhandledRequest: 'bypass',
+  });
+};
+
+enableMocking()
+  .then(() => {
+    createRoot(document.getElementById('app')!).render(
+      <StrictMode>
+        <BrowserRouter>
+          <QueryClientProvider client={queryClient}>
+            <App />
+          </QueryClientProvider>
+        </BrowserRouter>
+      </StrictMode>,
+    );
+  })
+  .catch((e: unknown) => console.error(e));

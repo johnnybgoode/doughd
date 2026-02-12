fd462aca4df0214a32250eb8d7bbb6e90768cefb
John Entwistle <j.entwistle76@gmail.com>
2026-02-12 17:19:03 -0500

    chore: apply formatting

--- packages/ui/src/components/image.tsx
+++ packages/ui/src/components/image.tsx
@@ -1,4 +1,4 @@
-import { useMediaQuery } from "../hooks/useMediaQuery";
+import { useMediaQuery } from '../hooks/useMediaQuery';
 
 const PREFERRED_IMAGE_WIDTH = 384;
 const MOBILE_PADDING = 16;
@@ -6,28 +6,28 @@ const ASPECT_RATIO_WIDTH = 16;
 const ASPECT_RATIO_HEIGHT = 9;
 
 type ImageProps = {
-	alt: string;
-	src: string;
-	width?: number;
+  alt: string;
+  src: string;
+  width?: number;
 };
 
 export function Image({ alt, src, width }: ImageProps) {
-	const isTabletAndUp = useMediaQuery("(min-width: 600px)");
+  const isTabletAndUp = useMediaQuery('(min-width: 600px)');
 
-	const imageWidth = Math.min(
-		width || PREFERRED_IMAGE_WIDTH,
-		window.innerWidth - MOBILE_PADDING,
-	);
-	const imageHeight = imageWidth / (ASPECT_RATIO_WIDTH / ASPECT_RATIO_HEIGHT);
+  const imageWidth = Math.min(
+    width || PREFERRED_IMAGE_WIDTH,
+    window.innerWidth - MOBILE_PADDING,
+  );
+  const imageHeight = imageWidth / (ASPECT_RATIO_WIDTH / ASPECT_RATIO_HEIGHT);
 
-	return (
-		<img
-			alt={alt}
-			decoding={isTabletAndUp ? "sync" : "async"}
-			height={imageHeight}
-			loading={isTabletAndUp ? "eager" : "lazy"}
-			src={src}
-			width={imageWidth}
-		/>
-	);
+  return (
+    <img
+      alt={alt}
+      decoding={isTabletAndUp ? 'sync' : 'async'}
+      height={imageHeight}
+      loading={isTabletAndUp ? 'eager' : 'lazy'}
+      src={src}
+      width={imageWidth}
+    />
+  );
 }
